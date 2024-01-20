7725e648546303cbb2447d03855f57897b07a642
Rednexie <126991413+Rednexie@users.noreply.github.com>
2024-01-20 18:37:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,17 @@ const sessionStorage = {
   setItem: (key, value) => _sessionStorage[key] = value,
   getItem: (key) => _sessionStorage[key],
   removeItem: (key) => delete _sessionStorage[key],
+  length: () => {
+    return JSON.stringify(_sessionStorage).length
+  },
+  key: (num) => {
+    return _sessionStorage[Object.keys(_sessionStorage).at(num)]
+  },
+  clear: () => {
+    for(key in _sessionStorage){
+      delete _sessionStorage[key]
+    }
+  },
 }
 
 module.exports = sessionStorage
